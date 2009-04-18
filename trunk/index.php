3c652c378832c1f804bd6b1efce03c884fd79484
deus.ex.13 <deus.ex.13@099a424a-2c19-11de-94d6-134fa40061eb>
2009-04-18 13:12:24 +0000

    Easy EventAI! v.0.1.2 Поиск скрипта по номеру и его загрузка в скрипт Вывод имени для НПС и ссылки на ВХ на них во всех полях, где есть их entry Всплывающие подсказки с ВХ для спеллов и ссылки для них на ВХ Оригинальный мануал EventAI.txt

---
+++ trunk/index.php
@@ -0,0 +1,64 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Language" content="ru">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style fprolloverstyle>A:hover {color: #FF0000}
+</style>
+
+<BODY text=#000000 leftMargin=0 topMargin=0 marginwidth="0" marginheight="0" bgcolor="#000000" rightmargin="0" bottommargin="0" link="#191970" alink="#8B008B" vlink="#8B008B">
+
+
+<table cellpadding="0" cellspacing="0" width="100%" height="100%">
+<!-- MSTableType="layout" -->
+
+<tr>
+<td valign="top">
+<div align="center">
+
+<table cellpadding="0" cellspacing="0" width="729">
+<!-- MSTableType="layout" -->
+
+<tr>
+<td height="360" background="images/header.jpg">&nbsp;</td>
+</tr>
+				
+<tr>
+<td background="images/border.jpg" height="622" valign="top">
+
+
+
+<div align="center">
+
+
+<br>
+<table cellpadding="0" cellspacing="0" width="570">
+	<!-- MSTableType="layout" -->
+	<tr>
+		<td>
+<?php
+include "easy_eventai.php";
+?>
+
+		</td>
+	</tr>
+</table>
+</div>
+</td>
+</tr>
+
+<tr>
+<td colspan="34" background="images/footer.jpg" height="117">&nbsp;</td>
+</tr>
+</table>
+
+<br>
+
+<font style="font-size: 9pt"><font color="#FFFFFF">©&nbsp;Blizzard Entertainment, Inc., 2008&nbsp;г.<br>
+Style by <span style="text-decoration: none"><b>D-SMP.BLADE</b></span><br>
+<?php echo $lang_title; ?></b> by <b>KiriX</b> v<b><?php echo $version; ?>. Thx, <b>Dragon</b></font></div>
+</font>
+<font size="1"><br>
+</td>
+</tr>
+</table>
